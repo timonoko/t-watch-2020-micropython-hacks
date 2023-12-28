04d6839379b3f3ef987bfca7e85297f8245f09ed
Timo Noko <timonoko@users.noreply.github.com>
2023-12-28 10:56:56 +0200

    lisänumeroita nurkkiin

--- src/clocks/nokoc.py
+++ src/clocks/nokoc.py
@@ -1,5 +1,5 @@
 from micropython import const
-from tempos import g, rtc, sched
+from tempos import g, rtc, sched, prtc, settings, pm
 from graphics import rgb, WHITE, BLACK, BLUE, CYAN, RED, YELLOW, GREEN
 from fonts import roboto24,roboto36
 import array
@@ -41,28 +41,37 @@ viikko= ["ma","ti","ke","to","pe","la","su"]
     
 dialpic = png.getPNG("images/nokodial.png", WHITE)
 
+def to_local(hrs):
+    hrs = hrs + settings.timezone
+    return hrs + 24 if hrs < 0 else hrs - 24 if hrs >= 24 else hrs
 
-def dialtext(text,x,y,big=False):
+def dialtext(text,x,y,big=False,color=GREY):
     ds = text
     if big: g.setfont(roboto36)
     else: g.setfont(roboto24)
     g.setfontalign(0, -1)
-    g.setcolor( GREY,BLACK)
+    g.setcolor(color,BLACK)
     g.text(ds,x,y)
     
 def dial(t):
     g.fill(WHITE)
     png.drawPNG(dialpic, 0, 0)
     dialtext(viikko[t[3]],20,0,True)
-    dialtext("{:04}".format(t[0]),210,0)
     dialtext("{:02}".format(t[2]),20,220)
-    dialtext(months[t[1] - 1],210,220)
     huu=t[4]%12
     if huu>2 and huu<10: dialtext("{:02}:{:02}".format(t[4],t[5]),120,65,True)
     if huu<4 or huu>8: dialtext("{:02}:{:02}".format(t[4],t[5]),120,145,True)
     if huu<1 or huu>5: dialtext("{:02}:{:02}".format(t[4],t[5]),170,120-12)
     if huu>11 or huu<7: dialtext("{:02}:{:02}".format(t[4],t[5]),70,120-12)
+    Ala,Min,Hou=prtc.read_alarm()
+    if Ala==1: dialtext("{:02}:{:02}".format(to_local(Hou),Min),210,0,False,YELLOW)
+    else: dialtext("{:04}".format(t[0]),210,0)
+    bat = pm.batPercent()
+    if bat < 25: dialtext("{:02}%".format(bat),210,220,False,YELLOW)
+    else: dialtext(months[t[1] - 1],210,220)
+        
 
+        
 def secH(a, c):
     drawRotRect(2, 3, R - 10, a, c)
 
